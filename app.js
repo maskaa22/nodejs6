817f3a776c518d4ca7f1522a61146440930862e0
MASHa <MA77777SHa>
2021-08-23 19:26:57 +0300

    home

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
+
 
 const {PORT} = require('./config/variables');
 const {loginRouter, userRouter, registrationRouter} = require('./routes');
